361b1e9ae13d0d25c5374442995a9a800efdc0a6
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2020-09-03 21:07:39 -0700

    Add files via upload

---
+++ optimism.py
@@ -0,0 +1,27 @@
+#create a variable 'name' and input intro conversation to ask user's name
+name = input("Hello, you beautiful Human. What's your name? ")
+
+gender = int(input("Well, HELLO " + name + "!!! My name is @nirdla_. You can find me on Instagram. \n May I say you look AMAZING today! \nAnyways, I am here to help you calculate exactly how many calories you need per day. \n First off, we need to calculate your Basal Metabolic Rate (BMR) \n To do that, I need to ask, are you: \n 1. MALE \n 2. FEMALE \n please choose 1 or 2: "))
+
+if gender == 1:
+        weight = float(input("Okay, cool. How much do you weigh in pounds?: "))
+        height = float(input("What is your height in inches?: "))
+
+
+#if gender = 1: #male
+    #weight = int(input("Okay, cool. How much do you weigh in pounds? Please round to who nearest whole number: "))
+    #height = int(input("What is your height in inches? Plea"))
+
+
+   # print("Oh okay. According to my calculations, your optimism level is at ", rating*age, "\nmeaning........... IT'S OVER 9,000")
+#if gender = 2: #female 
+ #   age = int(input("Hmm okay. Well, how old are you? "))
+  #  print("oh okay that's why")
+
+#meals = input("Oh wow... well how many meals did you eat today so far? ")
+
+
+#num1 = input("Enter the number of your choice: ")
+#num2 = input("Enter another number of your choice: ")
+#result = int(num1) + int(num2)
+#print(result)
